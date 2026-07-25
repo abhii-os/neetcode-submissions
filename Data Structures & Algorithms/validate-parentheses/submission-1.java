8a08bc66402509e72012eedff29e7c92b28506a2
Abhinav Sinha <abhinavsinha912@gmail.com>
2026-07-25 18:35:36 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '('|| ch=='{' || ch=='['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else{
+                    char top = stack.pop();
+                    if(ch==')'&& top != '(') return false;
+                    if(ch=='}'&& top != '{') return false;
+                    if(ch==']'&& top != '[') return false;  
+                }
+            }
+        }
+        return stack.isEmpty();
+        }
+}

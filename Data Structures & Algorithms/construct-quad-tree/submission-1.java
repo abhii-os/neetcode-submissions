3393452479dd4e674dd02f9719af074303e7f9cf
Abhinav Sinha <abhinavsinha912@gmail.com>
2026-07-29 16:26:53 +0530

    Add: construct-quad-tree - submission-1

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-1.java
@@ -0,0 +1,63 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return helper(grid,0,0,grid.length);
+    }
+    private Node helper(int[][] grid, int r, int c, int len){
+        if(len==1){
+            return new Node(grid[r][c]==1,true);
+        }
+        int half = len/2;
+        Node topLeft = helper(grid, r,c,half);
+        Node topRight = helper(grid,r,c+half,half);
+        Node bottomLeft = helper(grid,r+half,c,half);
+        Node bottomRight = helper(grid,r+half,c+half,half);
+
+        if(topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf
+        && topLeft.val == topRight.val
+        && topRight.val == bottomLeft.val
+        && bottomLeft.val == bottomRight.val){
+            return new Node(topLeft.val,true);
+        }
+        return new Node (true,false,topLeft,topRight,bottomLeft,bottomRight);
+    }
+}

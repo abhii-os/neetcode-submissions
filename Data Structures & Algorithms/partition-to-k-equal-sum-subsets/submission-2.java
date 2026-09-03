33d1b7b40ed4efc1b75503b825770df8fedfd4ad
Abhinav Sinha <abhinavsinha912@gmail.com>
2026-09-03 16:57:55 +0530

    Add: partition-to-k-equal-sum-subsets - submission-2

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for(int i:nums){
+            sum += i;
+        }
+        if(sum%k!=0 || nums.length==0)return false;
+        int target = sum/k;
+        Arrays.sort(nums);
+        reverse(nums);
+        if(nums[0]>target)return false;
+        int [] subsets = new int[k];
+        return backtrack(nums,0,subsets,target);
+    
+    }
+    private boolean backtrack(int[]nums, int index, int[] subsets, int target){
+        if(index==nums.length){
+            return true;
+        }
+        int curr = nums[index];
+
+        for(int i=0; i<subsets.length;i++){
+            if(subsets[i]+curr<=target){
+                subsets[i]+=curr;
+
+                if(backtrack(nums,index+1,subsets,target)) return true;
+                
+                subsets[i]-= curr;
+            }
+            if(subsets[i]==0)break;
+        }
+        return false;
+    }
+    private void reverse(int[]nums){
+        int left = 0, right = nums.length-1;
+        while(left<right){
+            int temp = nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
